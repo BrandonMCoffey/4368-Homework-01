2ee5c4d68a3b27d7cd50e6f36ea38d78751eac75
Brandon Coffey <bmc180001@utdallas.edu>
2021-08-25 14:10:47 -0500

    Adjust UI to have no in-scene references

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,29 +4,16 @@ using UnityEngine.UI;
 
 namespace Assets.Scripts {
     public class Inventory : MonoBehaviour {
-        [SerializeField] private Text _displayText = null;
-        [SerializeField] private string _textToDisplay = "Treasure: ";
         [SerializeField] private FloatReference _treasureCount = new FloatReference(0);
-
-        public int Treasure => Mathf.FloorToInt(_treasureCount.Value);
-
 
         private void OnEnable()
         {
             _treasureCount.Value = 0;
-            OnValueChanged();
         }
 
         public void AddTreasure(int amount = 1)
         {
             _treasureCount.Value += amount;
-            OnValueChanged();
-        }
-
-        private void OnValueChanged()
-        {
-            if (_displayText == null) return;
-            _displayText.text = _textToDisplay + _treasureCount;
         }
     }
 }
