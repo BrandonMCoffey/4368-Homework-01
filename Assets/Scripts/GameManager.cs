52da65b17e079c7d06b65471ed96b4a9a306143c
Brandon Coffey <bmc180001@utdallas.edu>
2021-08-26 17:59:37 -0500

    Game Level + Win System

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,30 @@
+using Assets.Scripts.Utility.CustomFloats;
+using Assets.Scripts.Utility.GameEvents.Logic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts {
     public class GameManager : MonoBehaviour {
         [SerializeField] private KeyCode _restartKey = KeyCode.Backspace;
+        [SerializeField] private FloatVariable _treasureCount = null;
+        [SerializeField] private int _treasureToWin = 10;
+        [SerializeField] private GameEvent _onWin = null;
 
         private void Update()
         {
             if (Input.GetKeyDown(_restartKey)) {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+
+            if (_treasureCount.Value >= _treasureToWin) {
+                Win();
+            }
+        }
+
+        private void Win()
+        {
+            if (_onWin != null) {
+                _onWin.Invoke();
             }
         }
     }
