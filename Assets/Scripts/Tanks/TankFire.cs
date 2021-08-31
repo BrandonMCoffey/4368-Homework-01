eb07dabef7b0844fef9b382914423c0cfa8089e0
Brandon Coffey <bmc180001@utdallas.edu>
2021-08-30 19:50:19 -0500

    Bullet Limit Option

--- Assets/Scripts/Tanks/TankFire.cs
+++ Assets/Scripts/Tanks/TankFire.cs
@@ -1,15 +1,29 @@
 using System.Collections.Generic;
+using System.Linq;
 using Assets.Scripts.Projectiles;
+using Assets.Scripts.Utility;
 using UnityEngine;
 
 namespace Assets.Scripts.Tanks {
     public class TankFire : MonoBehaviour {
+        [Header("Necessary Bullet Details")]
+        [SerializeField] private Transform _turretFirePos = null;
+        [SerializeField] private Bullet _bulletToFire = null;
+        [Header("Ignore Collision When Firing")]
         [SerializeField] private Collider _ignoreCollider = null;
         [SerializeField] private float _ignoreDuration = 0.2f;
-        [SerializeField] private Transform _turretEndPos = null;
-        [SerializeField] private Bullet _bulletToFire = null;
+        [Header("Limits")]
+        [SerializeField] private bool _hasMaximumBullets = false;
+        [SerializeField] private float _maximumBullets = 5;
 
         private List<Bullet> _bullets = new List<Bullet>();
+
+        private void Awake()
+        {
+            if (_bulletToFire == null || _turretFirePos == null) {
+                DebugHelper.Error("Missing Bullet Information To Fire");
+            }
+        }
 
         private void OnDisable()
         {
@@ -18,9 +32,12 @@ namespace Assets.Scripts.Tanks {
 
         public void Fire()
         {
-            if (_bulletToFire == null) return;
+            if (_bulletToFire == null || _turretFirePos == null) return;
 
-            Bullet bullet = Instantiate(_bulletToFire.gameObject, _turretEndPos.position, _turretEndPos.rotation).GetComponent<Bullet>();
+            _bullets = _bullets.Where(item => item != null).ToList();
+            if (_hasMaximumBullets && _bullets.Count >= _maximumBullets) return;
+
+            Bullet bullet = Instantiate(_bulletToFire.gameObject, _turretFirePos.position, _turretFirePos.rotation).GetComponent<Bullet>();
             if (_ignoreCollider != null) {
                 bullet.TemporaryIgnore(_ignoreCollider, _ignoreDuration);
             }
